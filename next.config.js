cd12a619667181b01fa6991187551c0164e06adc
AmirAbbasIT <amirsaeed624@gmail.com>
2023-03-06 07:25:44 +0500

    changes

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/home",
+        permanent: true,
+      },
+    ];
+  },
   reactStrictMode: true,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
