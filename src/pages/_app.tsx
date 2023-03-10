14655aab6cca807b56d06c5e7f0f313cef6363e1
AmirAbbasIT <amirsaeed624@gmail.com>
2023-03-10 18:10:47 +0500

    plans listed

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,22 +3,15 @@ import useFirebaseAuth from "@/hooks/useFirebaseAuth";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { AuthUserProvider } from "../context/firebaseContext";
-import { PlansProvider } from "../context/plansContext";
 import { useRouter } from "next/router";
 import VertexLoader from "@/components/loaders/VertexLoader";
 import PreLoadeer from "@/components/loaders/PreLoadeer";
 import "toastr/build/toastr.min.css";
-import Firebase, { getDb } from "@/configs/firebaseinit";
 import { StoreProvider } from "easy-peasy";
 import store from "@/store";
-import { useDispatch } from "react-redux";
-// import { getPlansData } from "@/store/slices/plans";
 
 export default function App({ Component, pageProps }: AppProps) {
   const { authUser, loading } = useFirebaseAuth();
-  // const dispatch = useDispatch();
-  // const {loading,blogs}=useStoreState(state=>state.policies)
-  // const {getBlogs}=useStoreActions(actions=>actions.policies)
   const router = useRouter();
 
   React.useEffect(() => {
